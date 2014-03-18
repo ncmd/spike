65864b6931ba9601bad978cc391f70e708c5bf6b
s.goodman <goodman@nginx-goodies.com>
2014-03-18 08:54:51 +0100

    settinsg/files!

---
+++ spike/model/settings.py
@@ -0,0 +1,14 @@
+from spike.model import db
+from time import time 
+
+class Settings(db.Model):
+  __bind_key__ = 'settings'
+  __tablename__ = 'settings'
+  
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(), nullable=False, unique = True)
+  value = db.Column(db.String(), nullable=False, server_default = "")
+
+  def __init__(self, name, value):
+    self.name = name 
+    self.value = value  
