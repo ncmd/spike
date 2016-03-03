eda929a8d037d22b7d007228e7a2e932050720f4
Julien (jvoisin) Voisin <jvo@nbs-system.com>
2016-03-03 11:58:01 +0100

    Simplify the markdown-magic handling

--- spike/views/__init__.py
+++ spike/views/__init__.py
@@ -1,14 +1,1 @@
-from os.path import isfile
-from markdown import markdown
-
 __all__ = ['default', 'rules', 'settings', 'docs']
-
-
-def render_md(md_file):
-    if not isfile(md_file):
-        return ''
-    return return_md("".join(open(md_file, "r").readlines()).decode("utf-8"))
-
-
-def return_md(md_input):
-    return markdown(md_input, extensions=['meta', 'extra', 'fenced_code', 'tables', 'codehilite', 'toc', 'attr_list'])
--- spike/views/docs.py
+++ spike/views/docs.py
@@ -1,22 +1,33 @@
 import os
-from glob import glob
+import glob
+import markdown
+import StringIO
 
 from flask import Blueprint, render_template, redirect
-from spike.views import render_md
 
 docs = Blueprint('docs', __name__, url_prefix='/docs')
 
 
+def __render_md(md_file):
+    """
+    :param str md_file: Path to a markdown file
+    :return str: html rendering of the `md_file` markdown file
+    """
+    if not os.path.isfile(md_file):
+        return ''
+    ret = StringIO.StringIO()
+    markdown.markdownFromFile(input=md_file, output=ret)
+    return ret.getvalue()
+
+
 @docs.route("/")
 def index():
-    return render_template("docs/index.html", doc_display=render_md("docs/docs.md"))
+    return render_template("docs/index.html", doc_display=__render_md("docs/docs.md"))
 
 
 @docs.route("/<path:doc_file>")
-def display(doc_file=0):
-    doc_files = glob("docs/*.md")
+def display(doc_file=''):
     doc_path = os.path.join("docs", doc_file)
-
-    if doc_path in doc_files:
-        return render_template("docs/display.html", display=render_md(doc_path))
+    if doc_path in glob.glob("docs/*.md"):
+        return render_template("docs/display.html", display=__render_md(doc_path))
     return redirect("/docs")
