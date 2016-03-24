809e83db633084bbd5444a13fa1b10f69da5a53e
Julien (jvoisin) Voisin <jvo@nbs-system.com>
2016-03-24 18:20:54 +0100

    Make the testsuite work again on an empty db

--- tests/test_sandbox.py
+++ tests/test_sandbox.py
@@ -12,7 +12,6 @@ except ImportError:  # python3
 
 import unittest
 
-
 class FlaskrTestCase(unittest.TestCase):
     def setUp(self):
         app = create_app()
@@ -20,6 +19,31 @@ class FlaskrTestCase(unittest.TestCase):
         app.config['TESTING'] = True
         self.app = app.test_client()
         self.created_rules = list()
+        self.__create_rule()
+
+    def tearDown(self):
+        self.__delete_rule()
+
+    def __create_rule(self):
+        """
+
+        :return int: The id of the new rule
+        """
+        current_sid = NaxsiRules.query.order_by(NaxsiRules.sid.desc()).first()
+        current_sid = 1337 if current_sid is None else current_sid.sid + 1
+
+        db.session.add(NaxsiRules(u'POUET', 'str:test', u'BODY', u'$SQL:8', current_sid, u'WEB_APPS',
+                                  u'f hqewifueiwf hueiwhf uiewh fiewh fhw', '1', True, 1457101045))
+        self.created_rules.append(current_sid)
+        return int(current_sid)
+
+    def __delete_rule(self, sid=None):
+        if sid:
+            db.session.delete(NaxsiRules.query.filter(sid == NaxsiRules.sid).first())
+        for sid in self.created_rules:
+            _rule = NaxsiRules.query.filter(sid == NaxsiRules.sid).first()
+            if _rule:
+                db.session.delete(_rule)
 
     def test_sandbox_rule(self):
         rv = self.app.get('/sandbox/rule')
