afe5c3ff6a11fac151ce2fb5c00866b5fabb83dc
Julien (jvoisin) Voisin <jvo@nbs-system.com>
2016-03-24 19:23:11 +0100

    Add a proto-atom feed!

--- spike/views/default.py
+++ spike/views/default.py
@@ -1,4 +1,8 @@
-from flask import Blueprint, redirect, Response, current_app, send_from_directory
+from flask import Blueprint, redirect, Response, current_app, send_from_directory, request
+from werkzeug.contrib.atom import AtomFeed
+from datetime import datetime
+
+from spike.model.naxsi_rules import NaxsiRules
 
 default = Blueprint('default', __name__)
 
@@ -16,3 +20,17 @@ def download():
 @default.route("/robots.txt")
 def robots():
     return Response('User-agent: *\n Disallow: /', mimetype='text/plain')
+
+
+@default.route('/rules.atom')
+def atom():
+    feed = AtomFeed('Recent rules', feed_url=request.url, url=request.url_root)
+    _rules = NaxsiRules.query.order_by(NaxsiRules.sid.desc()).limit(15).all()
+    if _rules:
+        for rule in _rules:
+            feed.add(title=rule.msg,
+                     entires=unicode(rule.fullstr()),
+                     content_type='text',
+                     updated=datetime.fromtimestamp(rule.timestamp),
+                     id=rule.sid)
+    return feed.get_response()
