7b0c38fdf82de661b6f835caefebc7185c2ab4fa
Julien (jvoisin) Voisin <jvo@nbs-system.com>
2016-03-30 18:24:48 +0200

    Add some tests

--- tests/test_whitelists.py
+++ tests/test_whitelists.py
@@ -1,12 +1,7 @@
 import unittest
-from time import strftime, localtime
-import re
 
 from spike import create_app
 from spike.model import db
-from spike.model.naxsi_whitelist import NaxsiWhitelist
-
-from tests import TestsThatNeedsRules
 
 try:
     from urlparse import urlparse
@@ -31,3 +26,27 @@ class FlaskrTestCase(unittest.TestCase):
     def test_new(self):
         rv = self.app.get('/whitelists/new')
         self.assertEqual(rv.status_code, 200)
+
+    def test_generate(self):
+        rv = self.app.get('/whitelists/generate')
+        self.assertEqual(rv.status_code, 200)
+
+        rv = self.app.post('/whitelists/generate')
+        self.assertEqual(rv.status_code, 200)
+        self.assertIn('Please input nxlogs', str(rv.data))
+
+        rv = self.app.post('/whitelists/generate', data={'nxlogs': 'pouet,lol'})
+        self.assertEqual(rv.status_code, 200)
+        self.assertIn('string &#34;ip=&#34; not found.', str(rv.data))
+
+        rv = self.app.post('/whitelists/generate', data={'nxlogs': 'ip=1234'})
+        self.assertEqual(rv.status_code, 200)
+        self.assertIn('string &#34;,&#34; not found.', str(rv.data))
+
+        logs = "2013/11/10 07:36:19 [error] 8278#0: *5932 NAXSI_FMT: ip=X.X.X.X&server=Y.Y.Y.Y&"\
+                "uri=/phpMyAdmin-2.8.2/scripts/setup.php&learning=0&vers=0.52&total_processed=472&total_blocked=204&"\
+                "block=0&cscore0=$UWA&score0=8&zone0=HEADERS&id0=42000227&var_name0=user-agent, client: X.X.X.X,"\
+                'server: blog.memze.ro, request: "GET /phpMyAdmin-2.8.2/scripts/setup.php HTTP/1.1", host: "X.X.X.X"'
+        rv = self.app.post('/whitelists/generate', data={'nxlogs': logs})
+        self.assertEqual(rv.status_code, 200)
+        self.assertIn('BasicRule wl:42000227 "mz:user-agent:HEADERS"', str(rv.data))
